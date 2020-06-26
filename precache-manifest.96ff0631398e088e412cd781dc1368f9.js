38d107939a15e9726e6aa8c027c75c0dfdd1c32f
killiandunne2 <killian.dunne97@gmail.com>
2020-06-26 12:38:11 +0100

    Updates

---
+++ precache-manifest.96ff0631398e088e412cd781dc1368f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e296899a7303897be99b776152534f68",
+    "url": "/dayMaker/index.html"
+  },
+  {
+    "revision": "fd0d19e634a7019588c0",
+    "url": "/dayMaker/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "bb12999fb8d5e096afdd",
+    "url": "/dayMaker/static/css/main.38daaea4.chunk.css"
+  },
+  {
+    "revision": "fd0d19e634a7019588c0",
+    "url": "/dayMaker/static/js/2.d146c3f6.chunk.js"
+  },
+  {
+    "revision": "691f985016ee4aa5c16e5d1595d12411",
+    "url": "/dayMaker/static/js/2.d146c3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb12999fb8d5e096afdd",
+    "url": "/dayMaker/static/js/main.9e0153ec.chunk.js"
+  },
+  {
+    "revision": "8298544d7f7f095a7b7a",
+    "url": "/dayMaker/static/js/runtime-main.ffd3aa49.js"
+  }
+]);
